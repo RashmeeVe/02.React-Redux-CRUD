2056b8cb5c34d7b7e8b708dc12130eb55b06dc72
RashmeeVe <83055419+RashmeeVe@users.noreply.github.com>
2021-05-17 17:50:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import store from "./store";
-import * as actions from "./actionTypes";
 import UserForm from "./UserForm";
 import UserDetails from "./UserDetails";
 import { withStyles } from "@material-ui/styles";
@@ -16,12 +14,6 @@ class App extends React.Component {
     this.state = { indexOfIdToEdit: -1, isUserFormDialogOpen: false };
   }
 
-  handleFormEntries = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-    return true;
-  };
-
   handleOpenUserFormDialog = () => {
     this.setState({
       isUserFormDialogOpen: true,
@@ -29,7 +21,7 @@ class App extends React.Component {
   };
 
   handleCloseUserFormDialog = () => {
-    this.setState({ isUserFormDialogOpen: false });
+    this.setState({ isUserFormDialogOpen: false, indexOfIdToEdit: -1 });
   };
 
   setStateValues = () => {
@@ -41,40 +33,6 @@ class App extends React.Component {
       indexToUpdateUser: -1,
     });
     this.handleOpenUserFormDialog();
-  };
-
-  addUser = (event) => {
-    event.preventDefault();
-    const { empCode, empName, empAge, empProfession } = this.state;
-
-    store.dispatch({
-      type: actions.ADD_USER,
-      payload: {
-        empCode: empCode,
-        empName: empName,
-        empAge: empAge,
-        empProfession: empProfession,
-      },
-    });
-    this.handleCloseUserFormDialog();
-  };
-
-  updateUser = (event) => {
-    event.preventDefault();
-    const { indexOfIdToEdit, empCode, empName, empAge, empProfession } =
-      this.state;
-    store.dispatch({
-      type: actions.UPDATE_USER,
-      payload: {
-        indexOfIdToEdit: indexOfIdToEdit,
-        empCode: empCode,
-        empName: empName,
-        empAge: empAge,
-        empProfession: empProfession,
-      },
-    });
-    this.handleCloseUserFormDialog();
-    this.setState({ indexOfIdToEdit: -1 });
   };
 
   editUser = (indexOfIdToEdit) => {
@@ -100,9 +58,6 @@ class App extends React.Component {
         {isUserFormDialogOpen && (
           <UserForm
             isUserFormDialogOpen={isUserFormDialogOpen}
-            handleFormEntries={this.handleFormEntries}
-            addUser={this.addUser}
-            updateUser={this.updateUser}
             indexOfIdToEdit={indexOfIdToEdit}
             handleCloseUserFormDialog={this.handleCloseUserFormDialog}
           />
